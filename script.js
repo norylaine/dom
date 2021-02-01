6c76e43f10b28cd4e674d01ed7ad1a62261c82b2
norylaine <norylaine@gmail.com>
2021-02-01 10:23:28 -0300

    incluir modal

--- script.js
+++ script.js
@@ -22,25 +22,67 @@ const inserirDados = (itens) => {
     imageFrame.appendChild(title);
     imageFrame.appendChild(description);
     div.appendChild(imageFrame);
-    div.onclick = () => mostrarModal(div)
+   
+    div.onclick = () => mostrarModal(div);
+   
     section.appendChild(div);
   }
 }
 
-const inserirModal = (div) => {
+const mostrarModal = (div) => {
   const event = div;
   
+  const container = document.querySelector('article');
+  const modal = document.querySelector('.modal');
+  
+  const url = document.createElement('a');
+  url.innerHTML = "clique no site do curso";
+  url.href = event.url;
+  url.setAttribute('target', '_blank');
+  modal.appendChild(url);
+
+  const button = document.createElement('button');
+  button. innerHTML = 'X';
+  modal.appendChild(button);
+
+  button.onclick = () => {
+    esconderModal(modal);
+    modal.innerHTML = "";
+  }
+  
+  container.classList.remove('oculta');
+  container.style.backgroundColor = "red";
+  container.style.width = "200px";
+  container.style.height = "200px";
+
+//  for(let i = 0; i < tags.length; i++){
+//    const tag = document.createElement('span')
+//    tag innerHTML = tags[i]
+//  };
+  
+const esconderModal = () => {
+  container.classList.add('oculta');
+}
+  
+}
+
+
+
+  /*
+  const inserirModal = (div) => {
+  const event = div;
   const url = document.createElement('a');
   url.innerHTML = 'clique no site do curso'
   url.href = event.url;
   url.setAttribute('target', '_blank');
   modal.appendChild(url);
 
-  //for(let i = 0; i < tags.length; i++){
-  //  const tag = document.createElement('span');
-  //  tag innerHTML = tags[i]
-  //};
-
 }
+*/ 
 
 inserirDados(data);
+
+
+  
+  
+  
